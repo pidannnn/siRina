e52732e29adfbaf7e2c56245585ec8a7581636bd
Fi'adn Fawzul Adzhim <fiadn.fawzul.adzhim@gmail.com>
2025-06-13 10:03:06 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './resources/**/*.jsx',
+    './resources/**/*.tsx',
+    './resources/**/*.ts',
+    './resources/**/*.html',
+    './resources/**/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
